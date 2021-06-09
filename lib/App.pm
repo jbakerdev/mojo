007dd1acc3524a3fe699136e0f212dcc1cf47fcf
Justin Baker <me@jbaker.dev>
2021-06-09 08:55:48 -0700

    Configure plugins
      * Mojolicious::Plugin::AutoReload
      * Mojolicious::Plugin::AssetPack
      * Mojolicious::Plugin::DBIC
      * Mojolicious::Plugin::OAuth2

--- lib/App.pm
+++ lib/App.pm
@@ -10,6 +10,24 @@ sub startup ($self) {
   # Configure the application
   $self->secrets($config->{secrets});
 
+  # Load plugins
+  $self->plugin('AssetPack' => { pipes => [qw(Sass Css Combine)] });
+  $self->plugin('AutoReload');
+  $self->plugin('DBIC' => $config->{dbic});
+  $self->plugin('OAuth2' => $config->{oauth2});
+  
+  # Configure assets
+  $self->asset->process(
+    # virtual name of the asset
+    "app.css" => (
+      # source files used to create the asset
+      "app.scss",
+    )
+  );
+
+  # Database
+  $self->schema->connect();
+
   # Router
   my $r = $self->routes;
 
